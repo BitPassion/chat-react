98a4942c14f37863184e5101a08c6d8614f12887
asherhoffman1120 <asher.hoffman1120@gmail.com>
2022-08-24 20:05:21 +0530

    added support to show LOCAL tag

--- components/ConversationsList.tsx
+++ components/ConversationsList.tsx
@@ -1,15 +1,14 @@
-import { classNames, truncate, formatDate, checkPath } from '../helpers'
+import { classNames, truncate, formatDate } from '../helpers'
 import Link from 'next/link'
 import Address from './Address'
 import { useRouter } from 'next/router'
-import { Conversation } from '@xmtp/xmtp-js/dist/types/src/conversations'
+import { Conversation } from '@xmtp/xmtp-js'
 import useConversation from '../hooks/useConversation'
 import { XmtpContext } from '../contexts/xmtp'
 import { Message } from '@xmtp/xmtp-js'
 import useEns from '../hooks/useEns'
 import Avatar from './Avatar'
-import { useContext, useEffect, useState } from 'react'
-import useXmtp from '../hooks/useXmtp'
+import { useContext } from 'react'
 
 type ConversationsListProps = {
   conversations: Conversation[]
@@ -113,34 +112,6 @@ const ConversationsList = ({
       getLatestMessage(convoBMessages)?.sent || new Date()
     return convoALastMessageDate < convoBLastMessageDate ? 1 : -1
   }
-  const { client } = useXmtp()
-  const [refresh, setRefresh] = useState(false)
-
-  const reloadIfQueryParamPresent = async () => {
-    if (checkPath()) {
-      const queryAddress = window.location.pathname.replace('/dm/', '')
-      if (queryAddress) {
-        const canMessage = await client?.canMessage(queryAddress)
-        const matchAddress = conversations.filter(
-          (convo) => queryAddress == convo.peerAddress
-        )
-        if (
-          canMessage ||
-          (Array.isArray(matchAddress) && matchAddress.length > 0)
-        ) {
-          router.push(window.location.pathname)
-          setRefresh(!refresh)
-        } else {
-          router.push('/')
-        }
-      }
-    }
-  }
-
-  useEffect(() => {
-    reloadIfQueryParamPresent()
-  }, [window.location.pathname])
-
   return (
     <div>
       {conversations &&
